907cf469682759df2f43b717e7b336655c197ec4
Pedro Rocha <pedrorocha73658@gmail.com>
2021-12-26 22:49:27 -0300

    Iniciando os estudos com c++ apredendo sobre variaveis

---
+++ aula5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415;
+
+int main(){
+
+    int number1 = 5, number2 = 29;
+
+    double number3 = 3.23;
+
+    float number4 = 3.98;
+
+    char letter = 'c';
+
+    string phrase = "Hello world";
+
+    cout << number1;
+    cout << "\n";
+    cout << number1 + number2;
+    cout << "\n";
+    cout << number3;
+    cout << "\n";
+    cout << number3 + number4;
+    cout << "\n";
+    cout << letter;
+    cout << "\n";
+    cout << phrase;
+    cout << "\n";
+    cout << pi;
+
+    return 0;
+}
