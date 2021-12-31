790e6de26ca6654ab42ee7ac1d7fb239d96858be
Pedro Rocha <pedrorocha73658@gmail.com>
2021-12-31 10:33:27 -0300

    Tratamento de exceções genéricas em C++

--- excecoes.cpp
+++ excecoes.cpp
@@ -13,12 +13,12 @@ int main(){
         if(age >= 18){      //dentro do bloco try, a condição vai ser testada,
             cout << "Access granted - you are old enough.\n";   //se não passar pela condição,
         }else{              
-            throw(age);     //o valor que deu erro vai ser lançado,
+            throw age;     //o valor que deu erro vai ser lançado,
         }
-    }catch(int number){//o parêmetro é o tipo de dado em que o throw manda, se não souber que tipo será
+    }catch(...){//o parêmetro é o tipo de dado em que o throw manda, se não souber que tipo será
                         //se coloca, "...", assim será recebido todos os tipo de dados
         cout << "Access denied - You must be at least 18 years old.\n";
-        cout << "Age is: " << number;   //e no bloco catch tentará resolver o problema,
+       //e no bloco catch tentará resolver o problema,
     }                                   //ou mostrará uma mensagem do erro.
 
 
